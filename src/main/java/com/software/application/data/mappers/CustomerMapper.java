c84244ac8bf1378877f5fc9e600deb55bc2eaff2
Chelariu Andrei-Cosmin <85230994+Chelariu-Cosmin@users.noreply.github.com>
2023-02-03 14:49:33 +0200

    implemented customer, registration mapper + customerDto

--- src/main/java/com/software/application/data/mappers/CustomerMapper.java
+++ src/main/java/com/software/application/data/mappers/CustomerMapper.java
@@ -1,4 +1,32 @@
 package com.software.application.data.mappers;
 
+import com.software.application.data.entity.Customer;
+import com.software.application.data.entity.Employee;
+import com.software.application.data.entity.dto.CustomerDTO;
+import com.software.application.data.entity.dto.EmployeeDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerMapper {
+
+    private final ModelMapper modelMapper;
+
+    public CustomerMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public CustomerDTO toDto(Customer customer){
+        modelMapper.getConfiguration()
+                   .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper.map(customer, CustomerDTO.class);
+
+    }
+
+    public Customer toCustomer(CustomerDTO customerDTO){
+        modelMapper.getConfiguration()
+                   .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper.map(customerDTO, Customer.class);
+    }
 }
--- src/main/java/com/software/application/data/mappers/RegistrationMapper.java
+++ src/main/java/com/software/application/data/mappers/RegistrationMapper.java
@@ -1,4 +1,32 @@
 package com.software.application.data.mappers;
 
+import com.software.application.data.entity.Employee;
+import com.software.application.data.entity.User;
+import com.software.application.data.entity.dto.EmployeeDTO;
+import com.software.application.data.entity.dto.RegistrationDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RegistrationMapper {
+
+    private final ModelMapper modelMapper;
+
+    public RegistrationMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public RegistrationDTO toDto(User user){
+        modelMapper.getConfiguration()
+                   .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper.map(user, RegistrationDTO.class);
+
+    }
+
+    public User toUser(RegistrationDTO registrationDTO){
+        modelMapper.getConfiguration()
+                   .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper.map(registrationDTO, User.class);
+    }
 }
